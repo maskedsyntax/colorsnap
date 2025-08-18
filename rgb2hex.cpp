e60396e668ef0ea6ed05aedeba5fda72e8a8c3aa
maskedsyntax <aftaab2507@gmail.com>
2025-08-19 01:37:10 +0530

    remove: old files

--- rgb2hex.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-std::string rgb2hex(int r, int g, int b)
-{
-    char hexcol[8];
-    snprintf(hexcol, sizeof(hexcol), "#%02X%02X%02X", r, g, b);
-    return std::string(hexcol);
-}
