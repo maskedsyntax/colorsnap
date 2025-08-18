0542a3c8710d0ddc914b50d94091a4142d5a376a
maskedsyntax <aftaab2507@gmail.com>
2025-08-19 01:01:47 +0530

    chore: move utilities to util folder

---
+++ utils/rgb2hex.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "rgb2hex.h"
+
+std::string rgb2hex(int r, int g, int b)
+{
+    char hexcol[8];
+    snprintf(hexcol, sizeof(hexcol), "#%02X%02X%02X", r, g, b);
+    return std::string(hexcol);
+}
+
+std::string rgb2hex(const RGB &color) 
+{ 
+    return rgb2hex(color.r, color.g, color.b); 
+}
